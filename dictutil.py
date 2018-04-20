bd1682a292d6f1759aac72e1aa729d7c9dda4214
smd <smdalton@ucsc.edu>
2018-04-20 11:41:21 -0700

    added simple dict utils

--- dictutil.py
+++ dictutil.py
@@ -1,4 +1,5 @@
 # Copyright 2013 Philip N. Klein
-def dict2list(dct, keylist): pass
+def dict2list(dct, keylist): return [dct[key] for key in keylist]
 
-def list2dict(L, keylist): pass
+def list2dict(L, keylist): return {keylist[i]:L[i] for i in range(len(keylist))}
+
